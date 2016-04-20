332ddcf4c7c2fbe11a714f23f38088ac51ce5173
Park June Chul <pjc0247@naver.com>
2016-04-20 15:20:16 +0900

    Create MyWaitForSeconds.cs

---
+++ src/MyWaitForSeconds.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class MyWaitForSeconds
+{
+    public float delay { get; private set; }
+
+    public MyWaitForSeconds(float delay)
+    {
+        this.delay = delay;
+    }
+}
